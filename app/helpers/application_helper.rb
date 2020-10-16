b197cd29c2727a0651f9ef071c6a9f4db037c53a
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-16 11:56:20 -0500

    adding style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def nav_btns
     if current_user
-      link_to('Profile', user_path(current_user), class: 'text-white text-decoration-none') + link_to('Log out', logout_path, class: 'text-white text-decoration-none')
+      link_to('Profile', user_path(current_user), class: 'text-dark text-decoration-none btn btn-light') + link_to('Log out', logout_path, class: 'text-dark text-decoration-none btn btn-light')
     else
-      link_to('Sign in', login_path, class: 'text-white text-decoration-none') + 'or' + link_to('Sign up', new_user_path, class: 'text-white text-decoration-none')
+      link_to('Sign in', login_path, class: 'text-dark text-decoration-none btn btn-light') + link_to('Sign up', new_user_path, class: 'text-dark text-decoration-none btn btn-light')
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
                                                 too_long: '200 characters in comment is the maximum allowed.', minimum: 3 }
 
   belongs_to :user
-  belongs_to :post
+  belongs_to :tweet
 end
