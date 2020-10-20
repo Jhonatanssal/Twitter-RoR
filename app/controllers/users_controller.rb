262d2f0e4321344838dcb125bdfbebc6a52b8001
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-20 15:13:18 -0500

    add login after successfully sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: 'User was successfully created.'
     else
       render 'new'
